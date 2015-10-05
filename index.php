ef36fe085ffe4bf2d78532343d824745c9c14e2b
= <thomas.darvik2@gmail.com>
2015-10-05 12:29:26 +0200

    Update

---
+++ index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>Proof</title>
+        <link href="style.css" type="text/css" rel="stylesheet">
+        <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
+        <script src="getImages.js"></script>
+    </head>
+    <body>
+
+        <div class="wrapper" id="wrapper">
+            <!-- images comes here -->
+        </div>
+
+
+    </body>
+</html>
