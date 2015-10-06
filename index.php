3f4215ef92b7bdb295c76b6e9a9b81c3c0f79f20
Christopher <friheim93@gmail.com>
2015-10-06 22:05:10 +0200

    chrisFace

--- index.php
+++ index.php
@@ -3,16 +3,96 @@
     <head>
         <meta charset="utf-8">
         <title>Proof</title>
+        <link rel="stylesheet" href="startbootstrap-3-col-portfolio-1.0.3">
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
         <link href="style.css" type="text/css" rel="stylesheet">
         <script src="//code.jquery.com/jquery-1.11.3.min.js"></script>
         <script src="getImages.js"></script>
     </head>
     <body>
 
-        <div class="wrapper" id="wrapper">
-            <!-- images comes here -->
+    <!--<div class="wrapper" id="wrapper">-->
+
+    <div id="search" class="input-group">
+      <input type="text" class="form-control" placeholder="Søk">
+      <span class="input-group-btn">
+        <button class="btn btn-default" type="button"><span class="glyphicon glyphicon-search" aria-hidden="true"></span></button>
+      </span>
+    </div>
+      
+        <div class="container">  
+            <div class="row">
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>                
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>                
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>
+            </div>
+            <div class="row">
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>                
+                <div class="col-md-4 portfolio-item">
+                    <img class="img-responsive" src="images/ye2.jpg">
+                    <h3>Picture name</h3>
+                    <p>Picture description</p>
+                </div>
+            </div>
         </div>
 
 
-    </body>
+ <!-- </body>-->
 </html>
+
+
+
+        <!--
+        <table id="pictureTable" class="wrapper">
+                <tr>
+                    <td><img class="img" src="images/img1.jpg"></td> <td><img class="img" src="images/img2.png"></td> <td><img class="img" src="images/img3.png"></td>
+                </tr>
+                <tr>
+                    <td><img class="img" src="images/img3.png"></td> <td><img class="img" src="images/img1.jpg"></td> <td><img class="img" src="images/img2.png"></td>
+                </tr>
+                <tr>
+                    <td><img class="img" src="images/img2.png"></td> <td><img class="img" src="images/img3.png"></td> <td><img class="img" src="images/img1.jpg"></td>
+                </tr>
+        </table>
+    </div>-->
+
+
+  
