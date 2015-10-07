14fc34a6efdc4ab69fa234e180b789c98f6ff6f0
= <thomas.darvik2@gmail.com>
2015-10-07 18:46:57 +0200

    removed old API folder and made a new one. Also made som changes to the class-file and setup.php file

--- API/test.php
+++ API/test.php
@@ -6,12 +6,13 @@
 	require_once("setup.php");
 	require_once("databasesystem.php");
 
-	$system = new DatabaseSystem("localhost",$localusername, $localpassword, "omnipicsdb");
+	$system = new DatabaseSystem("localhost",$local_username, $local_password, "omnipicsdb");
 	if($system->isOK() ==  1) {
 		print_r("SYSTEM IS OK<br>");
 	}
 
 	$data = $system->handleRequest($method, $req);
+
 /*
 	if($system->isOK() == 1) {
 		print_r("SYSTEM IS OK!<br>");
