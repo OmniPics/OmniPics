ad4a33967f957cc854395337d56c8143013bff92
= <thomas.darvik2@gmail.com>
2015-10-08 18:06:57 +0200

    Updated the JSON-files with 100 new datapoints

--- API/dump.php
+++ API/dump.php
@@ -2,8 +2,11 @@
 
     require_once("simpleAPI.php");
 
-    $me = new API("images.json");
+    $me = new API("images2.json");
     $me->loadFile();
     $me->getImage(1);
+    print_r("<br>");
+
+    $me->getRange(1,30);
 
 ?>
--- API/simpleAPI.php
+++ API/simpleAPI.php
@@ -22,6 +22,32 @@
             }
         }
 
+        /*
+            in range => between $first and $last
+            index >= $first || $index <= $last
+        */
+
+        public function getRange($first, $last) {
+            $result = Array();
+            foreach ($this->data as $indexOfImages => $imageArray) {
+                foreach($imageArray as $key => $value) {
+                    if($key == "id") {
+                        if($value >= $first && $value <= $last) {
+                            // the current value is in the range
+                            print_r("value in range: " . $value);
+                            print_r("<br>");
+                            array_push($result, $imageArray);
+                        } else {
+                            print_r("value not in range <br>");
+                        }
+                    } else {
+                        // the key is not ID, but filename or filetype
+                    }
+                }
+
+            }
+        }
+
         public function getImage($index) {
             $result = Array();
             foreach ($this->data as $indexOfImages => $imageArray) {
