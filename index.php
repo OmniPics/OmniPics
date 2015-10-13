f56ed5d133305695fd2bc2b6760835c5ec25e2d0
Christopher <friheim93@gmail.com>
2015-10-14 00:12:30 +0200

    started initial phase of picture viewing page

--- index.php
+++ index.php
@@ -8,12 +8,17 @@ session_start();
 
 $page = isset($_REQUEST["page"]) ? $_REQUEST["page"] : "";
 
+$picture_id = isset($_REQUEST["picture_id"]) ? $_REQUEST["picture_id"] : "";
+
 $pictures = new Picture($local_database, $local_username, $local_password);
 
 switch($page) {
 
-		case "dick":
-			echo "nio";
+		case "pictureViewer":
+			$pics = $pictures->listPictures();
+			$smarty->assign("pictures", $pics);
+			$smarty->assign("picture_id", $picture_id);
+			$smarty->display('pictureViewer.tpl');
 			break;
 
 		default:
