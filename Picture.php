2c4521fc0849d1f5f7c52eda7c6e22637ed7cefd
Tomasz Gliniecki <impartial92@gmail.com>
2015-10-16 17:21:35 +0200

    cleaned up Pictures

--- Picture.php
+++ Picture.php
@@ -1,18 +1,11 @@
 <?php
 
 class Picture {
-
-    // TODO : fix "place" variablel, either with meta, or some other shit
-
     private $place = "uis 24/7";
-
     private $picture_array;
     public $connection;
-
     function __construct($local_database, $local_username, $local_password) {
-
         $this->connection = mysqli_connect("localhost",$local_username,$local_password,$local_database);
-
         if (!mysqli_select_db($this->connection, $local_database)) {
             echo "unable to select pics: " . mysqli_error();
             exit;
@@ -22,8 +15,6 @@ class Picture {
             exit;
         }
     }
-
-
     function loadPicture($picture_id) {
         $picture = array(
             'picture_id' => 0,
@@ -34,7 +25,6 @@ class Picture {
             $sql = "SELECT *
                     FROM pictures
                     WHERE picture_id='$picture_id'";
-
             $result = mysqli_query($this->connection,$sql);
             if ($row = mysqli_fetch_assoc($result)) {
                 $picture = array(
@@ -48,15 +38,10 @@ class Picture {
             }
             return $picture;
         }
-
     }
-
-
     function listPictures($sql = "SELECT * FROM pictures") {
-
         $result = mysqli_query($this->connection,$sql);
         if (!$result) {echo "this shit here ($sql) didn't work" . mysqli_error($this->connection); exit;}
-        //if (mysqli_num_rows($result) == 0) {echo "this gallery is empty! BYE!";exit;}
         while($row = mysqli_fetch_assoc($result))
         {
             $this->picture_array[$row["picture_id"]] =  array(
@@ -77,11 +62,17 @@ class Picture {
         mysqli_free_result($result);
         return $new_array;
     }
-
-    // TODO : this is just temporary implementation, should be expanded
-
-
-
+    function sortedPictures($value, $order, $amount) {
+        if ($amount == 0) {$amount = "";}
+        else {$amount = "LIMIT $amount";}
+        if ($order == 0) {$order = "DESC";}
+        else if ($order > 0) {$order = "ASC";}
+        $sql = "SELECT *
+                FROM pictures
+                ORDER BY $value $order
+                $amount";
+        return $this->listPictures($sql);
+    }
     function addPicture($filename, $extension, $path) {
         // TODO: fix $this->place !!!
         $sql = "INSERT INTO pictures
@@ -94,47 +85,30 @@ class Picture {
             echo "err: " . $sql . "<br>" . $this->connection->error;
         }
     }
-
-
     function dumpDatabase(){
         $sql = "TRUNCATE TABLE ";
-
         if (mysqli_query($this->connection, $sql . "pictures;")!==TRUE) {echo "all WRONGED" ."pictures";}
         if (mysqli_query($this->connection, $sql . "meta;")!==TRUE) {echo "all WRONGED"."meta";}
         if (mysqli_query($this->connection, $sql . "has_meta;")!==TRUE) {echo "all WRONGED"."has_meta";}
         if (mysqli_query($this->connection, $sql . "has_album;")!==TRUE) {echo "all WRONGED"."has_album";}
         if (mysqli_query($this->connection, $sql . "album;")!==TRUE) {echo "all WRONGED"."album";}
-
         header('Location: '.'index.php');
     }
-
     function removePicture($picture_id){
         $sql = "
             DELETE FROM pictures
             WHERE picture_id=$picture_id";
-
         if (mysqli_query($this->connection, $sql)!==TRUE){
-            echo "failed at removeing file" . $sql;
+            echo "failed at removeing file " . $sql;
         }
-
         header('Location: '.'index.php');
     }
+    function addTag($tag) {
+        $sql = "INSERT INTO tags
+                (tags) VALUES ($tag)";
 
-    function getSortedBySQL($value, $order, $amount) {
-        if ($amount == 0) {$amount = "";}
-        else {$amount = "LIMIT $amount";}
-
-        if ($order == 0) {$order = "DESC";}
-        else if ($order > 0) {$order = "ASC";}
-
-        $sql = "SELECT *
-                FROM pictures
-                ORDER BY $value $order
-                $amount";
-
-        return $this->listPictures($sql);
+        if (mysqli_query($this->connection, $sql)!==TRUE){
+            echo "failed at inserting tag " . $sql;
+        }
     }
-
-    // TODO : add funcitons for EDITING pictures from database
-
 }
