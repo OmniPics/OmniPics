08388445f35dc7d4b393bd48805e11e2327ca5e4
elikoo <erikcong@hotmail.com>
2015-10-16 23:11:22 +0200

    ?

--- Picture.php
+++ Picture.php
@@ -1,29 +1,17 @@
 <?php
-
 class Picture {
-
-    // TODO : fix "place" variablel, either with meta, or some other shit
-
-    private $place = "uis 24/7";
-
+    private $tags;
     private $picture_array;
     public $connection;
-
     function __construct($local_database, $local_username, $local_password) {
-
         $this->connection = mysqli_connect("localhost",$local_username,$local_password,$local_database);
-
         if (!mysqli_select_db($this->connection, $local_database)) {
-            echo "unable to select pics: " . mysqli_error();
-            exit;
+            echo "unable to select pics: " . mysqli_error();exit;
         }
         if (!$this->connection) {
-            echo "unable to get inside: " . mysqli_error();
-            exit;
+            echo "unable to get inside: " . mysqli_error();exit;
         }
     }
-
-
     function loadPicture($picture_id) {
         $picture = array(
             'picture_id' => 0,
@@ -34,7 +22,6 @@ class Picture {
             $sql = "SELECT *
                     FROM pictures
                     WHERE picture_id='$picture_id'";
-
             $result = mysqli_query($this->connection,$sql);
             if ($row = mysqli_fetch_assoc($result)) {
                 $picture = array(
@@ -48,7 +35,6 @@ class Picture {
             }
             return $picture;
         }
-
     }
 
 
@@ -56,7 +42,6 @@ class Picture {
 
         $result = mysqli_query($this->connection,$sql);
         if (!$result) {echo "this shit here ($sql) didn't work" . mysqli_error($this->connection); exit;}
-        //if (mysqli_num_rows($result) == 0) {echo "this gallery is empty! BYE!";exit;}
         while($row = mysqli_fetch_assoc($result))
         {
             $this->picture_array[$row["picture_id"]] =  array(
@@ -77,16 +62,18 @@ class Picture {
         mysqli_free_result($result);
         return $new_array;
     }
-
-    // TODO : this is just temporary implementation, should be expanded
-
-
-
+    function sortedPictures($value, $order, $amount) {
+        if ($amount == 0) {$amount = "";}
+        else {$amount = "LIMIT $amount";}
+        if ($order == 0) {$order = "DESC";}
+        else if ($order > 0) {$order = "ASC";}
+        $sql = "SELECT * FROM pictures ORDER BY $value $order $amount";
+        return $this->listPictures($sql);
+    }
     function addPicture($filename, $extension, $path) {
         // TODO: fix $this->place !!!
-        $sql = "INSERT INTO pictures
-                (filename, extension, path, place, upload_date)
-                VALUES ('$filename', '$extension','$path','$this->place',NOW());";
+        $sql = "INSERT INTO pictures (filename, extension, path, place, upload_date)
+                VALUES ('$filename', '$extension','$path','abc',NOW());";
 
         if (mysqli_query($this->connection,$sql) === TRUE) {
             echo "picture is added to db";
@@ -94,29 +81,20 @@ class Picture {
             echo "err: " . $sql . "<br>" . $this->connection->error;
         }
     }
-
-
     function dumpDatabase(){
         $sql = "TRUNCATE TABLE ";
-
         if (mysqli_query($this->connection, $sql . "pictures;")!==TRUE) {echo "all WRONGED" ."pictures";}
         if (mysqli_query($this->connection, $sql . "meta;")!==TRUE) {echo "all WRONGED"."meta";}
         if (mysqli_query($this->connection, $sql . "has_meta;")!==TRUE) {echo "all WRONGED"."has_meta";}
         if (mysqli_query($this->connection, $sql . "has_album;")!==TRUE) {echo "all WRONGED"."has_album";}
         if (mysqli_query($this->connection, $sql . "album;")!==TRUE) {echo "all WRONGED"."album";}
-
         header('Location: '.'index.php');
     }
-
     function removePicture($picture_id){
-        $sql = "
-            DELETE FROM pictures
-            WHERE picture_id=$picture_id";
-
+        $sql = "DELETE FROM pictures WHERE picture_id=$picture_id";
         if (mysqli_query($this->connection, $sql)!==TRUE){
-            echo "failed at removeing file" . $sql;
+            echo "failed at removeing file " . $sql;
         }
-
         header('Location: '.'index.php');
     }
 
@@ -133,8 +111,28 @@ class Picture {
                 $amount";
 
         return $this->listPictures($sql);
+    function hasTag($tag){
+        // TODO: logic for checking if tag exists in db. in SQL!
     }
+    function getTags(){
+        // TODO: return array of all the tags with id's
+    }
+    function addTag($tag,$picture_id) {
+        if(!$this->hasTag($tag)){
+            $sql = "INSERT INTO tags (tags) VALUES ($tag)";
+            if (mysqli_query($this->connection, $sql)!==TRUE){
+                echo "failed at inserting tag " . $sql;
+            }
+        }
+        $sql = "SELECT tags_id FROM tags WHERE tags='$tag'";
+        $result = mysqli_query($this->connection, $sql);
+        if ($result!==TRUE){
+            echo "failed at getting tagID wtf " . $sql;
+        }
+        while($row = mysqli_fetch_assoc($result)){$tags_id = $row['tags_id'];}
 
-    // TODO : add funcitons for EDITING pictures from database
-
+        $insert = "INSERT INTO has_tags (picture_id, tags_id)
+                   VALUES ($picture_id,$tags_id)";
+        mysqli_query($this->connection,$insert);
+    }
 }
--- index.php
+++ index.php
@@ -1,5 +1,4 @@
 <?php
-
 require("setup.php");
 require("Picture.php");
 require("smartyStarter.php");
@@ -7,23 +6,12 @@ require("smartyStarter.php");
 session_start();
 
 $page = isset($_REQUEST["page"]) ? $_REQUEST["page"] : "";
-
 $picture_id = isset($_REQUEST["picture_id"]) ? $_REQUEST["picture_id"] : "";
-
 $arrayOfPictureIDs = isset($_REQUEST["selectedPictures"]) ? $_REQUEST["selectedPictures"] : "";
 
 $pictures = new Picture($local_database, $local_username, $local_password);
 
-
-
 switch($page) {
-
-	case "dumpdb":
-		$pictures->dumpDatabase();
-		// TODO: smarty stuff....
-
-		break;
-
 	case "pictureViewer":
 		$pics = $pictures->listPictures();
 		$smarty->assign("pictures", $pics);
@@ -40,11 +28,12 @@ switch($page) {
 		$smarty->display('frontPage.tpl');
 		break;
 
+
+
 	case "removePics":
 		foreach ($arrayOfPictureIDs as $key) {
 			$pictures->removePicture($key);
 		}
-
 	default:
 		$pics = $pictures->listPictures();
 		$smarty->assign("selected", $arrayOfPictureIDs);
