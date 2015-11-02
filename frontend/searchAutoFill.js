4f721a671b4b0880298a2e789bd9e2b74b02b442
Tomasz Gliniecki <impartial92@gmail.com>
2015-11-02 15:55:27 +0100

    styling updated

--- frontend/searchAutoFill.js
+++ frontend/searchAutoFill.js
@@ -4,10 +4,11 @@
 
 function showResult(tagArray){
     // TODO: generate media div's for search, how many?
-    var tag = "<a>"+tagArray+"</a>";
-    var media = "<div class='media-object'><img src='images/thumbs/3366ff.png' alt='...'>"+tag+"</div>";
-    var media2 = "<div class='media-object'><img src='images/thumbs/images.jpeg' alt='...'>"+tag+"</div>";
-    document.getElementById("livesearch").innerHTML = media + media2;
+    var media = "<div class='media-object'><img class='thumb-image' src='images/thumbs/2iHDjiM.jpg' alt='...'>"+tagArray+"</div>";
+    var media2 = "<div class='media-object'><img class='thumb-image' src='images/thumbs/My Snapshot99.jpg' alt='...'>"+tagArray+"</div>";
+    var media3 = "<div class='media-object'><img class='thumb-image' src='images/thumbs/snek_irl.png' alt='...'>"+tagArray+"</div>";
+    var media4 = "<div class='media-object'><img class='thumb-image' src='images/thumbs/3366ff.png' alt='...'>"+tagArray+"</div>";
+    document.getElementById("livesearch").innerHTML = media + media2 + media3 + media4;
 }
 
 function hideResult() {
