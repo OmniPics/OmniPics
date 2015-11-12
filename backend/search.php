f014cd0b54be1cf34482ce458130bbe6cdded442
tokams <t.gliniecki@hotmail.com>
2015-11-12 11:33:57 +0100

    fixed up in files prepare for search

--- backend/search.php
+++ backend/search.php
@@ -1,7 +1,14 @@
 <?php
 /**
- * Created by PhpStorm.
  * User: tomasz
  * Date: 30.10.15
  * Time: 13:27
  */
+ require("../setup.php");
+ require("../Picture.php");
+ function searchPictures($tagArray){
+  $sql = "";
+  $con = new Picture($local_database, $local_username, $local_password);
+  $picArray =  $con->listPictures($sql);
+
+ }
