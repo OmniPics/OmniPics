327a1670519a5c2b9da05c04e65d820999d2b03c
tokams <t.gliniecki@hotmail.com>
2015-11-12 20:13:21 +0100

    backend search funcion type OR

--- backend/search.php
+++ backend/search.php
@@ -4,11 +4,42 @@
  * Date: 30.10.15
  * Time: 13:27
  */
- require("../setup.php");
- require("../Picture.php");
- function searchPictures($tagArray){
-  $sql = "";
-  $con = new Picture($local_database, $local_username, $local_password);
-  $picArray =  $con->listPictures($sql);
+require_once("Picture.php");
 
+function searchPictures($keywordsArray,$con){
+ $pictures = array();
+ $orsDes = "";
+ $orsTag = "";
+ foreach ($keywordsArray as  $key) {
+   $orsDes .= "OR description LIKE '%".$key."%' ";
  }
+ foreach ($keywordsArray as  $key) {
+   $orsTag .= "OR tags LIKE '%".$key."%' ";
+ }
+ $sql = "
+ SELECT
+ pictures.picture_id
+ FROM has_tags
+      INNER JOIN pictures
+        ON pictures.picture_id=has_tags.picture_id
+      INNER JOIN tags
+        ON tags.tags_id=has_tags.tags_id
+      INNER JOIN has_meta
+        ON has_meta.picture_id=has_tags.picture_id
+      INNER JOIN meta
+        ON meta.meta_id=has_meta.meta_id
+ WHERE description LIKE '' ".
+ //append OR's
+ $orsDes . $orsTag
+ ."GROUP BY pictures.picture_id;
+  ";
+
+  $result = mysqli_query($con->connection, $sql);
+  $foundPictures = array();
+  while($row = mysqli_fetch_assoc($result)) {
+      $foundPictures[] = $con->loadPicture($row['picture_id']);
+  }
+
+  return $foundPictures;
+
+}
