7053e7fb0c548aac27860bcfa1c0a36784d55b31
thomasdarvik2 <t.darvik@gmail.com>
2015-11-15 12:50:23 +0100

    change made to bulk from meta

--- backend/bulkUpload.php
+++ backend/bulkUpload.php
@@ -2,6 +2,7 @@
 require("../setup.php");
 require("../Picture.php");
 require("functions.php");
+require("../meta/app.php");
 define("UPLOAD_DIR", "../images/");
 
 $picture = new Picture($local_database, $local_username, $local_password);
@@ -41,6 +42,11 @@ if(!empty($_FILES['myPic']['name'][0])) {
             $file_dir = "images/" . $filename . "." . $pieces[1];
             $uploaded[$position] = $file_dir;
           if(move_uploaded_file($file_tmp, "../".$file_dir)) {
+
+            $metaSystem = new Metaclass($file_dir);
+            $currentMeta = $metaSystem->getMeta();
+
+
             createThumbnail($filename.".".$pieces[1]);
             $uploaded[$position] = $file_dir;
           } else {
